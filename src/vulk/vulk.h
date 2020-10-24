51bac81efe439b7287ca25834945e555fe0f0126
Sami Alameddine <sala0599@stu.gusd.net>
2020-10-24 19:54:57 +0000

    Patch: More Vulkan Boilerplate

--- src/vulk/vulk.h
+++ src/vulk/vulk.h
@@ -1,6 +1,7 @@
 #ifndef ALSAMI_VULKAN_MAIN_HEADER_OPENGL
 #define ALSAMI_VULKAN_MAIN_HEADER_OPENGL
 
+VkInstance vulk_init;
 
 /*
  *      ALSGM CODENAME YIN
@@ -19,6 +20,12 @@
 
 const uint32_t WIDTH=800;
 const uint32_t HEIGHT=600;
+uint32_t glfwExtensionCount=0;
+const char glfwExtensions;
+
+int vern1;
+int vern2;
+int vern3;
 
 void win_init(){
         // Tells GLFW to not create an OpenGL context
@@ -26,6 +33,36 @@ void win_init(){
         glgwWindowHint(GLFW_RESIZABLE,GLFW_FALSE);
 
 
+}
+
+void vulk_inst_create(){
+  VkApplicationInfo vulk_appinfo{};
+  vulk_appinfo.sType-VK_STRUCTURE_TYPE_APPLICATION_INFO;
+  vulk_appinfo.pApplicationInfo= vulk_als_appnm;
+  appInfo.applicationVersion=VK_MAKE_VERSION(vern1,vern2,vern3);
+  appInfo.pEngineName="alsgm-vulk";
+  appInfo.engineVersion=VK_MAKE_VERSION(1,2,9);
+  appInfo.apiVersion=VK_API_VERSION_1_0;
+  vkInstanceCreateInfo vulk_cinfo{};
+  vulk_cinfo.sType=VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+  vulk_cinfo.pApplicationInfo=&vulk_appinfo;
+
+}
+
+void vulk_init(){
+  vulk_inst_create();
+
+}
+
+void vulk_wlp(){
+  while(!glfwWindowShouldClose(window)){
+    glfwPollEvents();
+  }
+}
+
+void vulk_cleanup(){
+  glfwDestroyWindow(window);
+  glfwTerminate();
 }
 
 void vulk_run(){

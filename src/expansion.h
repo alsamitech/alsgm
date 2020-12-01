f55e82b7b6281c26aa4ccc3395ae75b84d93ca70
Sami Alameddine <sami@als.fyi>
2020-11-30 22:22:14 -0600

    bug fixes

--- src/expansion.h
+++ src/expansion.h
@@ -1,20 +1,21 @@
 #ifndef ALSAMI_GAME_ENGINE_EXPANDER_API_HDR
 #define ALSAMI_GAME_ENGINE_EXPANDER_API_HDR
 
-#include <ihdr.h>
+#include "ihdr.h"
 
-void module_id_assign(Module* mod_i){
-}
 
 /*WARNING TO ALSGM MODULE DEVELOPERS!
  *DO NOT PLAY WITH THE ASSGN_MODULEID_COUNT VARIABLE! However, it is kept public for the sake of ID handling extensions*/
 
 class Module{
   public:
-    virtual Module(){}
+    Module(){}
     virtual ~Module(){}
     unsigned int MODULE_ID;
     uint16_t assgn_moduleid_count;
 };
+
+void module_id_assign(Module* mod_i){
+}
 
 #endif
